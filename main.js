8b297d9b303240807f0fd6244a80d0b3327d99c4
Tyler Brow <tymuska@yahoo.com>
2019-03-23 11:47:46 -0700

    changing sidebar html

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var itemcontainer = document.querySelector("#itemcontainer")
 
 var htmlstr = items.map(item => {
    return `<div class="items">
-     <img id="image" src="${unescape(item.Images[0].url_75x75)}" />
+     <img id="image" src="${unescape(item.Images[0].url_fullxfull)}" />
      <p class="name">${item.title.substring(0, 28)}</p>
      <p class="whomade">${item.Shop.shop_name}</p>
      <p class="price">$${item.price}</p>
